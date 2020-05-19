1c1aad08af3b5984089c01b4cf9e653c665e4356
Aaron Schuyler <asky28@gmail.com>
2020-05-19 23:57:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,8 @@
 class Transfer
-  # your code here
+  attr_accessor :account_1, :account_2, :ammount
+  def initialize(account_1, account_2, ammount)
+    @account_1 = account_1
+    @account_2 = account_2
+    @ammount = ammount
+  end
 end
