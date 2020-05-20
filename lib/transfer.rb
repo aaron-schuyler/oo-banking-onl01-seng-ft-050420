90f5ac5682142757b60b187e0376d0692bcff08a
Aaron Schuyler <asky28@gmail.com>
2020-05-20 00:10:29 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,8 @@ class Transfer
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.balance + @amount
       @status = "complete"
-    else
+    elsif @sender.balance < @amount
+      @status = "regected"
       "Transaction rejected. Please check your account balance."
     end
   end
