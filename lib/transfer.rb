42207665eea02fe00dadc118c1d8c73481225198
Aaron Schuyler <asky28@gmail.com>
2020-05-20 00:09:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,6 +14,8 @@ class Transfer
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.balance + @amount
       @status = "complete"
+    else
+      
     end
   end
 end
